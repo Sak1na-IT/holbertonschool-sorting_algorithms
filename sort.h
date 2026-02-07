34e33401ce97f513d873e043a896facbc35ad778
Sak1na-IT <pasayevasekine844@gmail.com>
2026-02-07 11:49:39 +0000

    python

--- sort.h
+++ sort.h
@@ -1,6 +1,22 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stddef.h>
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-#endif
+
+/**
+ * struct listint_s - doubly linked list node
+ * @n: integer
+ * @prev: points to the previous element
+ * @next: points to the next element
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+
+#endif /* SORT_H */
