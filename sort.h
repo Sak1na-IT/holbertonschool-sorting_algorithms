153451fb368280649c2e5afa1e00506cf27984de
Sak1na-IT <pasayevasekine844@gmail.com>
2026-02-07 12:48:18 +0000

    python

--- sort.h
+++ sort.h
@@ -1,7 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stddef.h>
-void print_array(const int *array,size_t size);
-void swap(int *a,int *b);
-void quick_sort(int *array,size_t size);
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const int_list_t *list);
+
+/* Quick Sort Prototipləri */
+void quick_sort(int *array, size_t size);
+void swap_items(int *array, size_t size, int *a, int *b);
+int lomuto_partition(int *array, size_t size, int low, int high);
+void quick_recursion(int *array, size_t size, int low, int high);
+
 #endif
