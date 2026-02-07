09679463bcbcecbe9c6aaf44007f8438e5a0464e
Sak1na-IT <pasayevasekine844@gmail.com>
2026-02-07 12:44:41 +0000

    python

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,61 +1,74 @@
 #include "sort.h"
 
-/* Swap two integers */
-void swap(int *a, int *b)
+/**
+ * swap_items - İki elementin yerini dəyişir və massivi çap edir
+ * @array: Massiv
+ * @size: Massivin ölçüsü
+ * @a: Birinci element
+ * @b: İkinci element
+ */
+void swap_items(int *array, size_t size, int *a, int *b)
 {
-	if (a != b)
-	{
-		int tmp = *a;
-		*a = *b;
-		*b = tmp;
-	}
+if (*a != *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+print_array(array, size);
+}
 }
 
-/* Lomuto partition scheme */
-int lomuto_partition(int *array, int low, int high, size_t size)
+/**
+ * lomuto_partition - Lomuto sxemi ilə massivi hissələrə ayırır
+ * @array: Massiv
+ * @size: Massivin ölçüsü
+ * @low: Alt hissənin başlanğıcı
+ * @high: Alt hissənin sonu
+ * Return: Pivotun indeksi
+ */
+int lomuto_partition(int *array, size_t size, int low, int high)
 {
-	int pivot = array[high];
-	int i = low, j;
+int pivot = array[high];
+int i = low;
+int j;
 
-	for (j = low; j < high; j++)
-	{
-		if (array[j] < pivot)
-		{
-			if (i != j)
-			{
-				swap(&array[i], &array[j]);
-				print_array(array, size);
-			}
-			i++;
-		}
-	}
-
-	if (i != high)
-	{
-		swap(&array[i], &array[high]);
-		print_array(array, size);
-	}
-
-	return i;
+for (j = low; j < high; j++)
+{
+if (array[j] < pivot)
+{
+swap_items(array, size, &array[i], &array[j]);
+i++;
+}
+}
+swap_items(array, size, &array[i], &array[high]);
+return (i);
 }
 
-/* Recursive quick sort */
-void quick_sort_recursive(int *array, int low, int high, size_t size)
+/**
+ * quick_recursion - Rekursiv sıralama funksiyası
+ * @array: Massiv
+ * @size: Massivin ölçüsü
+ * @low: Başlanğıc indeks
+ * @high: Son indeks
+ */
+void quick_recursion(int *array, size_t size, int low, int high)
 {
-	if (low < high)
-	{
-		int p = lomuto_partition(array, low, high, size);
-
-		quick_sort_recursive(array, low, p - 1, size);
-		quick_sort_recursive(array, p + 1, high, size);
-	}
+if (low < high)
+{
+int pivot_idx = lomuto_partition(array, size, low, high);
+quick_recursion(array, size, low, pivot_idx - 1);
+quick_recursion(array, size, pivot_idx + 1, high);
+}
 }
 
-/* Entry function */
+/**
+ * quick_sort - Quick Sort alqoritminin əsas funksiyası
+ * @array: Massiv
+ * @size: Massivin ölçüsü
+ */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
-
-	quick_sort_recursive(array, 0, size - 1, size);
+if (!array || size < 2)
+return;
+quick_recursion(array, size, 0, size - 1);
 }
