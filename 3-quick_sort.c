fd81a273ef3bb6891f5ff2cbb48a494e9784de88
Sak1na-IT <pasayevasekine844@gmail.com>
2026-02-07 12:40:31 +0000

    python

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,37 +1,61 @@
 #include "sort.h"
-void swap(int *a,int *b){
-if(a!=b){
-int tmp=*a;
-*a=*b;
-*b=tmp;
+
+/* Swap two integers */
+void swap(int *a, int *b)
+{
+	if (a != b)
+	{
+		int tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
 }
+
+/* Lomuto partition scheme */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low, j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+
+	if (i != high)
+	{
+		swap(&array[i], &array[high]);
+		print_array(array, size);
+	}
+
+	return i;
 }
-int lomuto_partition(int *array,int low,int high,size_t size){
-int pivot=array[high];
-int i=low,j;
-for(j=low;j<high;j++){
-if(array[j]<pivot){
-if(i!=j){
-swap(&array[i],&array[j]);
-print_array(array,size);
+
+/* Recursive quick sort */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int p = lomuto_partition(array, low, high, size);
+
+		quick_sort_recursive(array, low, p - 1, size);
+		quick_sort_recursive(array, p + 1, high, size);
+	}
 }
-i++;
-}
-}
-if(i!=high){
-swap(&array[i],&array[high]);
-print_array(array,size);
-}
-return i;
-}
-void quick_sort_recursive(int *array,int low,int high,size_t size){
-if(low<high){
-int p=lomuto_partition(array,low,high,size);
-quick_sort_recursive(array,low,p-1,size);
-quick_sort_recursive(array,p+1,high,size);
-}
-}
-void quick_sort(int *array,size_t size){
-if(!array||size<2)return;
-quick_sort_recursive(array,0,size-1,size);
+
+/* Entry function */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
 }
