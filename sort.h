2f2a0984932ab0a275022d0d6c105df602e23d87
Sak1na-IT <pasayevasekine844@gmail.com>
2026-02-07 12:49:26 +0000

    python

--- sort.h
+++ sort.h
@@ -4,10 +4,25 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void print_array(const int *array, size_t size);
-void print_list(const int_list_t *list);
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
-/* Quick Sort Prototipləri */
+/* Helper functions provided by the project */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Quick Sort Prototypes */
 void quick_sort(int *array, size_t size);
 void swap_items(int *array, size_t size, int *a, int *b);
 int lomuto_partition(int *array, size_t size, int low, int high);
